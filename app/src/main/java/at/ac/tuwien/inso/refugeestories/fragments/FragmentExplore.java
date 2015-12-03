f45f6d1fe84b5818901904512e0c7ba72c1c324c
Michael Traxler <michaeltraxler@gmx.net>
2015-12-03 12:16:56 +0100

    Centered HorizontalScroll

--- app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentExplore.java
+++ app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentExplore.java
@@ -2,14 +2,17 @@ package at.ac.tuwien.inso.refugeestories.fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -30,10 +33,24 @@ public class FragmentExplore extends Fragment {
 
     private RelativeLayout mFragmentLayout;
 
+    private float itemWidth;
+    private float padding;
+    private float firstItemWidth;
+    private float allPixels;
+    private int mLastPosition;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         mFragmentLayout = (RelativeLayout) inflater.inflate(R.layout.fragment_explore, container, false);
+
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        itemWidth = getResources().getDimension(R.dimen.item_width);
+        padding = (size.x - itemWidth) / 2;
+        firstItemWidth = getResources().getDimension(R.dimen.padding_item_width);
+        allPixels = 0;
 
         mRecyclerView = (RecyclerView) mFragmentLayout.findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -50,6 +67,25 @@ public class FragmentExplore extends Fragment {
                     }
                 }));
 
+        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                synchronized (this) {
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        calculatePositionAndScroll(recyclerView);
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                allPixels += dx;
+            }
+        });
+
         return mFragmentLayout;
     }
 
@@ -62,6 +98,26 @@ public class FragmentExplore extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         this.context = activity;
+    }
+
+
+    private void calculatePositionAndScroll(RecyclerView recyclerView) {
+        int expectedPosition = Math.round((allPixels + padding - firstItemWidth) / itemWidth);
+        // Special cases for the padding items
+        if (expectedPosition == -1) {
+            expectedPosition = 0;
+        } else if (expectedPosition >= recyclerView.getAdapter().getItemCount() - 2) {
+            expectedPosition--;
+        }
+        scrollListToPosition(recyclerView, expectedPosition);
+    }
+
+    private void scrollListToPosition(RecyclerView recyclerView, int expectedPosition) {
+        float targetScrollPos = expectedPosition * itemWidth + firstItemWidth - padding;
+        float missingPx = targetScrollPos - allPixels;
+        if (missingPx != 0) {
+            recyclerView.smoothScrollBy((int) missingPx, 0);
+        }
     }
 }
 /*
