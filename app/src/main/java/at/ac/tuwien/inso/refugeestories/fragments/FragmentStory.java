550f4b42a3fb020bc89ee4ec9dbf5e5f432e6e5b
Vorschi <e1026777@student.tuwien.ac.at>
2015-12-09 14:58:32 +0100

    Beginning of story creating

--- app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentStory.java
+++ app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentStory.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +12,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import at.ac.tuwien.inso.refugeestories.R;
-import at.ac.tuwien.inso.refugeestories.domain.Person;
-import at.ac.tuwien.inso.refugeestories.utils.MockFactory;
 
 /**
  * Created by Amer Salkovic on 14.11.2015.
@@ -38,7 +35,6 @@ public class FragmentStory extends Fragment {
             }
         });
         return contentView;
-
     }
 
     @Override
