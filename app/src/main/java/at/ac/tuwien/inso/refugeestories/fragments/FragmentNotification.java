e0024991308b70a4152e73928c51236072694ca0
Vorschi <e1026777@student.tuwien.ac.at>
2016-01-13 10:29:00 +0100

    notifications-screen, pictures added

--- app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentNotification.java
+++ app/src/main/java/at/ac/tuwien/inso/refugeestories/fragments/FragmentNotification.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import at.ac.tuwien.inso.refugeestories.R;
 import at.ac.tuwien.inso.refugeestories.utils.Consts;
@@ -17,10 +19,41 @@ import at.ac.tuwien.inso.refugeestories.utils.Consts;
 public class FragmentNotification extends Fragment {
 
     private Context context;
+    RelativeLayout but1;
+    RelativeLayout but2;
+    RelativeLayout but3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        but1 = (RelativeLayout)contentView.findViewById(R.id.not_but1);
+        but1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //TODO redirect to the latest story of Sami Turku
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), "JAAAAAAAAAAAAAAAAAAAAAAAAAAAA", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        but2 = (RelativeLayout)contentView.findViewById(R.id.not_but2);
+        but2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //TODO redirect to latest story of Stefan Mayr
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), "JAAAAAAAAAAAAAAAAAAAAAAAAAAAA", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        but3 = (RelativeLayout)contentView.findViewById(R.id.not_but3);
+        but3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //TODO redirect to your own timeline
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), "JAAAAAAAAAAAAAAAAAAAAAAAAAAAA", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
         return contentView;
     }
@@ -34,6 +67,10 @@ public class FragmentNotification extends Fragment {
     public static FragmentNotification getInstance() {
         FragmentNotification f = new FragmentNotification();
         return f;
+    }
+
+    public void tastk1(View view){
+
     }
 
     public String getName(){
